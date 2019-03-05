8e53c04bb299f07ab43da59657ddd4f6cca40c75
shi.zhaozhong@cern.ch <zzshi@mit.edu>
2019-03-05 14:25:08 +0100

    second commit

--- QualityControl/Modules/Skeleton/include/Skeleton/SkeletonTask.h
+++ QualityControl/Modules/Skeleton/include/Skeleton/SkeletonTask.h
@@ -9,44 +9,135 @@
 
 #include "QualityControl/TaskInterface.h"
 
+#include <vector>
+#include <deque>
+#include <memory>
+#include "Rtypes.h"		// for Digitizer::Class, Double_t, ClassDef, etc
+#include "TObject.h"		// for TObject
+#include "FairTask.h"
+
+
+
+#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/DigitPixelReader.h"
+
+
+#include "DataFormatsITSMFT/ROFRecord.h"
+#include "SimulationDataFormat/MCCompLabel.h"
+//#include "/home/alidock/alice/O2/Detectors/ITSMFT/common/simulation/include/ITSMFTSimulation/Digitizer.h"
+//#include "/home/alidock/alice/O2/Detectors/ITSMFT/common/base/include/ITSMFTBase/SegmentationAlpide.h"
+#include <fstream>
+#include "Framework/DataProcessorSpec.h"
+#include "Framework/Task.h"
+#include "ITSMFTReconstruction/Clusterer.h"
+#include "uti.h"
+
+//#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/ITS/base/include/ITSBase/GeometryTGeo.h"
+//#include "DetectorsBase/GeometryManager.h"
+//#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/common/base/include/ITSMFTBase/GeometryTGeo.h"
+
+#include "ITSMFTReconstruction/DigitPixelReader.h"
+#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/ITS/QCWorkFlow/include/ITSQCWorkflow/HisAnalyzerSpec.h"
+
+
+
+using namespace o2::framework;
+using namespace o2::ITSMFT;
+
 class TH1F;
 
 using namespace o2::quality_control::core;
 
 namespace o2
 {
-namespace quality_control_modules
-{
-namespace skeleton
-{
+	namespace quality_control_modules
+	{
+		namespace skeleton
+		{
 
-/// \brief Example Quality Control DPL Task
-/// It is final because there is no reason to derive from it. Just remove it if needed.
-/// \author Barthelemy von Haller
-/// \author Piotr Konopka
-class SkeletonTask /*final*/ : public TaskInterface // todo add back the "final" when doxygen is fixed
-{
- public:
-  /// \brief Constructor
-  SkeletonTask();
-  /// Destructor
-  ~SkeletonTask() override;
+			/// \brief Example Quality Control DPL Task
+			/// It is final because there is no reason to derive from it. Just remove it if needed.
+			/// \author Barthelemy von Haller
+			/// \author Piotr Konopka
+			class SkeletonTask /*final*/ : public TaskInterface // todo add back the "final" when doxygen is fixed
+			{
 
-  // Definition of the methods for the template method pattern
-  void initialize(o2::framework::InitContext& ctx) override;
-  void startOfActivity(Activity& activity) override;
-  void startOfCycle() override;
-  void monitorData(o2::framework::ProcessingContext& ctx) override;
-  void endOfCycle() override;
-  void endOfActivity(Activity& activity) override;
-  void reset() override;
+				using ChipPixelData = o2::ITSMFT::ChipPixelData;
+				using PixelReader = o2::ITSMFT::PixelReader;	
+				public:
+				/// \brief Constructor
+				SkeletonTask();
+				/// Destructor
+				~SkeletonTask() override;
+				inline void setDigits (std::vector < o2::ITSMFT::Digit > *dig);
+				std::vector<o2::ITSMFT::Digit> mDigitsArray;                     
+/				std::vector<o2::ITSMFT::Digit>* mDigitsArrayPtr = &mDigitsArray; 
+				UInt_t getCurrROF() const { return mCurrROF; }
+				void setNChips(int n)
+				{
+					mChips.resize(n);
+					mChipsOld.resize(n);
+				}
 
- private:
-  TH1F* mHistogram;
-};
 
-} // namespace skeleton
-} // namespace quality_control_modules
+				// Definition of the methods for the template method pattern
+				void initialize(o2::framework::InitContext& ctx) override;
+				void startOfActivity(Activity& activity) override;
+				void startOfCycle() override;
+				void monitorData(o2::framework::ProcessingContext& ctx) override;
+				void process (PixelReader& r);
+				void endOfCycle() override;
+				void endOfActivity(Activity& activity) override;
+				void reset() override;
+//				o2::ITSMFT::GeometryTGeo* gm = o2::ITSMFT::GeometryTGeo::Instance();
+
+
+
+				private:
+				TH1F* mHistogram;
+
+				ChipPixelData* mChipData = nullptr; 
+				std::vector<ChipPixelData> mChips;
+				std::vector<ChipPixelData> mChipsOld;
+				o2::ITSMFT::PixelReader* mReader = nullptr; 
+				std::unique_ptr<o2::ITSMFT::DigitPixelReader> mReaderMC;    
+				std::unique_ptr<o2::ITSMFT::RawPixelReader<o2::ITSMFT::ChipMappingITS>> mReaderRaw; 
+				UInt_t mCurrROF = o2::ITSMFT::PixelData::DummyROF; 
+				int* mCurr; // pointer on the 1st row of currently processed mColumnsX
+				int* mPrev; // pointer on the 1st row of previously processed mColumnsX
+				static constexpr int   NCols = 1024;
+				static constexpr int   NRows = 512;
+				static constexpr int   NPixels = NRows*NCols;
+				const int NLay1 = 108;
+				const int NEventMax = 20;
+				double Occupancy[108];
+				int lay, sta, ssta, mod, chip;
+				TH2D * ChipStave = new TH2D("ChipStave","ChipStave",NLay1,0,NLay1,NEventMax,0,NEventMax);
+				TH1D * ChipProj = new TH1D("ChipProj","ChipProj",NLay1,0,NLay1);
+				o2::ITS::GeometryTGeo * gm = o2::ITS::GeometryTGeo::Instance();
+
+				void swapColumnBuffers()
+				{
+					int* tmp = mCurr;
+					mCurr = mPrev;
+					mPrev = tmp;
+				}
+
+				void resetColumn(int* buff)
+				{
+					std::memset(buff, -1, sizeof(int) * NRows);
+
+				}
+
+				const std::string inpName = "itsdigits.root";
+
+				double AveOcc;
+				UShort_t ChipID; 
+				int ActPix;
+
+			};
+
+		} // namespace skeleton
+	} // namespace quality_control_modules
 } // namespace o2
 
 #endif // QC_MODULE_SKELETON_SKELETONTASK_H
--- QualityControl/Modules/Skeleton/src/SkeletonTask.cxx
+++ QualityControl/Modules/Skeleton/src/SkeletonTask.cxx
@@ -4,113 +4,255 @@
 /// \author Piotr Konopka
 ///
 
+#include <sstream>
+
+#include <TStopwatch.h>
+#include "DataFormatsParameters/GRPObject.h"
+#include "FairLogger.h"
+#include "FairRunAna.h"
+#include "FairFileSource.h"
+#include "FairRuntimeDb.h"
+#include "FairParRootFileIo.h"
+#include "FairSystemInfo.h"
+#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/ITS/QCWorkFlow/include/ITSQCWorkflow/HisAnalyzerSpec.h"
+//#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/common/base/include/ITSMFTBase/GeometryTGeo.h"
+
+//#include "ITSSimulation/DigitizerTask.h"
+//#include "../include/ITSQCWorkflow/HisAnalyzerSpec.h"
+
+
+#include "/data/zhaozhong/alice/O2/Detectors/ITSMFT/common/reconstruction/include/ITSMFTReconstruction/DigitPixelReader.h"
+#include "DetectorsBase/GeometryManager.h"
+//#include "ITSBase/GeometryTGeo.h"
+
 #include <TCanvas.h>
-#include <TH1.h>
+
 
 #include "QualityControl/QcInfoLogger.h"
 #include "Skeleton/SkeletonTask.h"
 
+
+
+using o2::ITSMFT::Digit;
+using Segmentation = o2::ITSMFT::SegmentationAlpide;
+
+
+using namespace std;
+using namespace o2::ITSMFT;
+using namespace o2::ITS;
+
+
+
+
 namespace o2
 {
-namespace quality_control_modules
-{
-namespace skeleton
-{
+	namespace quality_control_modules
+	{
+		namespace skeleton
+		{
 
-SkeletonTask::SkeletonTask() : TaskInterface(), mHistogram(nullptr) { mHistogram = nullptr; }
+			gSystem->Load();
+			//		o2::Base::GeometryManager::loadGeometry();
 
-SkeletonTask::~SkeletonTask() {
-  if (mHistogram) {
-    delete mHistogram;
-  }
-}
+			SkeletonTask::SkeletonTask() : TaskInterface(), mHistogram(nullptr) { mHistogram = nullptr; }
 
-void SkeletonTask::initialize(o2::framework::InitContext& ctx)
-{
-  QcInfoLogger::GetInstance() << "initialize SkeletonTask" << AliceO2::InfoLogger::InfoLogger::endm;
+			SkeletonTask::~SkeletonTask() {
+				if (mHistogram) {
+					delete mHistogram;
+				}
+			}
 
-  mHistogram = new TH1F("example", "example", 20, 0, 30000);
-  getObjectsManager()->startPublishing(mHistogram);
-  getObjectsManager()->addCheck(mHistogram, "checkFromSkeleton", "o2::quality_control_modules::skeleton::SkeletonCheck",
-                                "QcSkeleton");
-}
+			void SkeletonTask::initialize(o2::framework::InitContext& ctx)
+			{
+				QcInfoLogger::GetInstance() << "initialize SkeletonTask" << AliceO2::InfoLogger::InfoLogger::endm;
 
-void SkeletonTask::startOfActivity(Activity& activity)
-{
-  QcInfoLogger::GetInstance() << "startOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
-  mHistogram->Reset();
-}
+				//auto filename = ctx.options().get < std::string > ("itsdigits.root");
 
-void SkeletonTask::startOfCycle()
-{
-  QcInfoLogger::GetInstance() << "startOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
-}
+				//std::unique_ptr<TFile> mFile = nullptr;
+				//mFile = std::make_unique<TFile>(filename.c_str(), "OLD");
 
-void SkeletonTask::monitorData(o2::framework::ProcessingContext& ctx)
-{
-  // In this function you can access data inputs specified in the JSON config file, for example:
-  //  {
-  //    "binding": "random",
-  //    "dataOrigin": "ITS",
-  //    "dataDescription": "RAWDATA"
-  //  }
+				//			QcInfoLogger::GetInstance() << "Input File Name is " << filename.c_str () <<  AliceO2::InfoLogger::InfoLogger::endm;
+				QcInfoLogger::GetInstance() << "It WORK, we start plotting histograms" <<  AliceO2::InfoLogger::InfoLogger::endm;
 
-  // Use Framework/DataRefUtils.h or Framework/InputRecord.h to access and unpack inputs (both are documented)
-  // One can find additional examples at:
-  // https://github.com/AliceO2Group/AliceO2/blob/dev/Framework/Core/README.md#using-inputs---the-inputrecord-api
 
-  // Some examples:
+				std::vector<ChipPixelData> mChips;
+				std::vector<ChipPixelData> mChipsOld;
+				o2::ITSMFT::PixelReader* mReader = nullptr; 
+				std::unique_ptr<o2::ITSMFT::DigitPixelReader> mReaderMC;   
+				const std::string inpName = "itsdigits.root";
+				bool mRawDataMode = 0;
+				if (mRawDataMode)
+				{
 
-  // 1. In a loop
-  for (auto&& input : ctx.inputs()) {
-    // get message header
-    const auto* header = header::get<header::DataHeader*>(input.header);
-    // get payload of a specific input, which is a char array.
-//    const char* payload = input.payload;
+					int a = 1;
+				}
+				else
+				{				
+					mReaderMC = std::make_unique < o2::ITSMFT::DigitPixelReader > ();
+					mReader = mReaderMC.get ();
+				}
 
-    // for the sake of an example, let's fill the histogram with payload sizes
-    mHistogram->Fill(header->payloadSize);
-  }
+				o2::ITS::GeometryTGeo * geom = o2::ITS::GeometryTGeo::Instance ();
+				geom->fillMatrixCache (o2::utils::bit2Mask (o2::TransformType::L2G));	
 
-  // 2. Using get("<binding>")
+				QcInfoLogger::GetInstance() << "It WORK, PASS 3" <<  AliceO2::InfoLogger::InfoLogger::endm;
 
-  // get the payload of a specific input, which is a char array. "random" is the binding specified in the config file.
-//   auto payload = ctx.inputs().get("random").payload;
+				const Int_t numOfChips = geom->getNumberOfChips ();
 
-  // get payload of a specific input, which is a structure array:
-//  const auto* header = header::get<header::DataHeader*>(ctx.inputs().get("random").header);
-//  struct s {int a; double b;};
-//  auto array = ctx.inputs().get<s*>("random");
-//  for (int j = 0; j < header->payloadSize / sizeof(s); ++j) {
-//    int i = array.get()[j].a;
-//  }
+				QcInfoLogger::GetInstance() << "numOfChips = " << numOfChips <<  AliceO2::InfoLogger::InfoLogger::endm;
 
-  // get payload of a specific input, which is a root object
-//   auto h = ctx.inputs().get<TH1F*>("histos");
-//   Double_t stats[4];
-//   h->GetStats(stats);
-//   auto s = ctx.inputs().get<TObjString*>("string");
-//   LOG(INFO) << "String is " << s->GetString().Data();
-}
+				setNChips (numOfChips);
+				int IndexNow = 0;
 
-void SkeletonTask::endOfCycle()
-{
-  QcInfoLogger::GetInstance() << "endOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
-}
+				QcInfoLogger::GetInstance() << "START LOOPING BRO" <<  AliceO2::InfoLogger::InfoLogger::endm;
 
-void SkeletonTask::endOfActivity(Activity& activity)
-{
-  QcInfoLogger::GetInstance() << "endOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
-}
+				mReaderMC->openInput ("itsdigits.root", o2::detectors::DetID ("ITS"));
 
-void SkeletonTask::reset()
-{
-  // clean all the monitor objects here
+				while (mReaderMC->readNextEntry())
+				{
+					QcInfoLogger::GetInstance()  << "Now Working on Event = " << IndexNow << AliceO2::InfoLogger::InfoLogger::endm;
+					process (*mReader);
+					IndexNow = IndexNow + 1;
+				}
 
-  QcInfoLogger::GetInstance() << "Resetting the histogram" << AliceO2::InfoLogger::InfoLogger::endm;
-  mHistogram->Reset();
-}
 
-} // namespace skeleton
-} // namespace quality_control_modules
+
+
+				mHistogram = new TH1F("example", "example", 20, 0, 30000);
+				getObjectsManager()->startPublishing(mHistogram);
+				getObjectsManager()->addCheck(mHistogram, "checkFromSkeleton", "o2::quality_control_modules::skeleton::SkeletonCheck",
+						"QcSkeleton");
+
+			}
+
+			void SkeletonTask::process (PixelReader & reader)
+			{
+
+
+				QcInfoLogger::GetInstance()  << "START PROCESSING" <<  AliceO2::InfoLogger::InfoLogger::endm;
+
+
+
+				for (int i = 0; i < NLay1; i++)
+				{
+					Occupancy[i] = 0;
+				}
+				//cout << "START MCHIPDATA" << endl;
+				while ((mChipData = reader.getNextChipData (mChips)))
+				{
+					cout << "ChipID Before = " << ChipID << endl; 
+					ChipID = mChipData->getChipID ();
+
+					gm->getChipId (ChipID, lay, sta, ssta, mod, chip);
+					// lay =  gm->GetLayer(ChipID);
+					// sta =  gm->getStave(ChipID);
+
+					if (lay < 1)
+					{
+
+						QcInfoLogger::GetInstance()  << "ChipID = " << ChipID <<  AliceO2::InfoLogger::InfoLogger::endm;
+						ActPix = mChipData->getData ().size ();
+
+						Occupancy[ChipID] = Occupancy[ChipID] + ActPix;
+					}
+				}
+				QcInfoLogger::GetInstance()  << "Start Filling" <<  AliceO2::InfoLogger::InfoLogger::endm;
+				for (int i = 0; i < NLay1; i++)
+				{
+					int XBin = ChipStave->GetXaxis ()->FindBin (i);
+					AveOcc = Occupancy[i] / NPixels;
+					ChipStave->Fill (XBin, Occupancy[i]);
+				}
+
+
+
+
+
+			}
+
+
+
+
+			void SkeletonTask::startOfActivity(Activity& activity)
+			{
+				QcInfoLogger::GetInstance() << "startOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
+				mHistogram->Reset();
+			}
+
+			void SkeletonTask::startOfCycle()
+			{
+				QcInfoLogger::GetInstance() << "startOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
+			}
+
+			void SkeletonTask::monitorData(o2::framework::ProcessingContext& ctx)
+			{
+				// In this function you can access data inputs specified in the JSON config file, for example:
+				//  {
+				//    "binding": "random",
+				//    "dataOrigin": "ITS",
+				//    "dataDescription": "RAWDATA"
+				//  }
+
+				// Use Framework/DataRefUtils.h or Framework/InputRecord.h to access and unpack inputs (both are documented)
+				// One can find additional examples at:
+				// https://github.com/AliceO2Group/AliceO2/blob/dev/Framework/Core/README.md#using-inputs---the-inputrecord-api
+
+				// Some examples:
+
+				// 1. In a loop
+				for (auto&& input : ctx.inputs()) {
+					// get message header
+					const auto* header = header::get<header::DataHeader*>(input.header);
+					//	const auto*  headername = header::get<header::HeaderType*>(input.header);
+
+					mHistogram->Fill(header->payloadSize);
+
+					//QcInfoLogger::GetInstance() << "header = "  <<  headername <<  AliceO2::InfoLogger::InfoLogger::endm;
+
+				}
+
+				// 2. Using get("<binding>")
+
+				// get the payload of a specific input, which is a char array. "random" is the binding specified in the config file.
+				//   auto payload = ctx.inputs().get("random").payload;
+
+				// get payload of a specific input, which is a structure array:
+				//  const auto* header = header::get<header::DataHeader*>(ctx.inputs().get("random").header);
+				//  struct s {int a; double b;};
+				//  auto array = ctx.inputs().get<s*>("random");
+				//  for (int j = 0; j < header->payloadSize / sizeof(s); ++j) {
+				//    int i = array.get()[j].a;
+				//  }
+
+				// get payload of a specific input, which is a root object
+				//   auto h = ctx.inputs().get<TH1F*>("histos");
+				//   Double_t stats[4];
+				//   h->GetStats(stats);
+				//   auto s = ctx.inputs().get<TObjString*>("string");
+				//   LOG(INFO) << "String is " << s->GetString().Data();
+			}
+
+			void SkeletonTask::endOfCycle()
+			{
+				QcInfoLogger::GetInstance() << "endOfCycle" << AliceO2::InfoLogger::InfoLogger::endm;
+			}
+
+			void SkeletonTask::endOfActivity(Activity& activity)
+			{
+				QcInfoLogger::GetInstance() << "endOfActivity" << AliceO2::InfoLogger::InfoLogger::endm;
+			}
+
+			void SkeletonTask::reset()
+			{
+				// clean all the monitor objects here
+
+				QcInfoLogger::GetInstance() << "Resetting the histogram" << AliceO2::InfoLogger::InfoLogger::endm;
+				mHistogram->Reset();
+			}
+
+		} // namespace skeleton
+	} // namespace quality_control_modules
 } // namespace o2
+
+
+
